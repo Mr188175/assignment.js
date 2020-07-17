349b549848a9a83d6c572469298a8d8a709ad9f5
Mr188175 <rahman188175@gmail.com>
2020-07-17 10:52:45 +0600

    feetToMile done

---
+++ assignment.js
@@ -0,0 +1,11 @@
+// Assignment feetToMile start 
+
+function feetToMile(feet) {
+    var mile = feet / 5280;
+    return mile;
+}
+
+var myFeet = feetToMile(500000);
+console.log(myFeet);
+
+// Assignment feetToMile end
