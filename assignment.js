c67b2b541725fd58ba0bdc25b7552ba80a98d1c6
Mr188175 <rahman188175@gmail.com>
2020-07-17 12:05:01 +0600

    woodCalculator done

--- assignment.js
+++ assignment.js
@@ -9,3 +9,27 @@ var myFeet = feetToMile(500000);
 console.log(myFeet);
 
 // Assignment feetToMile end
+
+
+
+// Assignment woodCalculator start
+
+function woodCalculator(chair,table,bed) {
+   var chairCount = chair * 1;
+   var tableCount = table * 3;
+   var bedCount = bed * 5;
+   var total = chairCount + tableCount + bedCount;
+   return total;
+}
+
+var totalWood = woodCalculator(10,5,2);
+console.log(totalWood);
+
+
+
+
+
+
+
+
+
